c195c7a562a6619bd3521b7ab09cc265a2580077
Chris Reid <chris@raygun6.com>
2015-02-04 19:47:01 -0800

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: 'Family Guy', description: 'A very funny show.', small_cover_url: '', large_cover_url: '')
+Video.create(title: 'Futurama', description: 'Another funny show. Please insert girder.', small_cover_url: '', large_cover_url: '')
+Video.create(title: 'Monk', description: 'Never watched this one.', small_cover_url: '', large_cover_url: '')
+Video.create(title: 'South Park', description: 'Is this show still on?', small_cover_url: '', large_cover_url: '')
