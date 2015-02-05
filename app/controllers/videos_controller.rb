462eb8a3f5bd88cdcad19795b5287629ddb9d038
Chris Reid <chris@raygun6.com>
2015-02-05 12:06:59 -0800

    adds boilerplate code for actions

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,21 @@
 class VideosController < ApplicationController
+  #before_action :set_video, only: [:show]
 
   def index
-
+    @videos = Video.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+  # def post_params
+  #   params.require(:video).permit(:id)
+  # end
 
 end
