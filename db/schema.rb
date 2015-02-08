587c09b185df1faac4e9ef05eed64f6439c97b76
Chris Reid <chris@raygun6.com>
2015-02-08 14:58:09 -0800

    adds initial categroy model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205012307) do
+ActiveRecord::Schema.define(version: 20150208225310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "videos", force: true do |t|
     t.string "title"
