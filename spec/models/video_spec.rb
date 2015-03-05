3dbedddfcf7db1a97e277a6a7401cdb20a424340
Chris Reid <chris@raygun6.com>
2015-03-05 14:20:31 -0800

    adds test for video category relationship

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,12 @@ describe Video do
     video.save
     expect(Video.first.title).to eq('Space War')
   end
+
+  it "belongs to a category" do
+    scifi = Category.create(name: 'Sci-Fi')
+    space_war = Video.create(title: 'Space War',
+      description: 'Best film of 1972',
+      category: scifi)
+    expect(space_war.category).to eq(scifi)
+  end
 end
