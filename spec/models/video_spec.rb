ccf589c216872d812e1af8cfa0b3d47686d4908b
Chris Reid <chris@raygun6.com>
2015-03-06 16:59:04 -0800

    adds validations for video title, decription

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -14,4 +14,16 @@ describe Video do
       category: scifi)
     expect(space_war.category).to eq(scifi)
   end
+
+  it "requires a title" do
+    space_war = Video.new(description: 'Best film of 1972')
+    space_war.valid?
+    expect(space_war.errors.messages[:title]).to include("can't be blank")
+  end
+
+  it "requires a description" do
+    space_war = Video.new(title: 'Space War')
+    space_war.valid?
+    expect(space_war.errors.messages[:description]).to include("can't be blank")
+  end
 end
