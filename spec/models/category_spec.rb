2dcb75d9f98f5bf25cbc86556438c2d85d546697
Chris Reid <chris@raygun6.com>
2015-03-26 15:27:09 -0700

    switches tests to use shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,21 +2,6 @@ require 'spec_helper'
 
 describe Category do
 
-  it "saves itself" do
-    category = Category.new(name: 'News')
-    category.save
-    expect(Category.first.name).to eq('News')
-  end
-
-  it "has many videos" do
-    scifi = Category.create(name: 'Sci-Fi')
-    space_war = Video.create(title: 'Space War',
-      description: 'Best film of 1972',
-      category: scifi)
-    space_war_2 = Video.create(title: 'Space War II',
-      description: 'Best film of 1974',
-      category: scifi)
-    expect(scifi.videos).to eq([space_war, space_war_2])
-  end
+  it { should have_many(:videos) }
 
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,29 +1,9 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: 'Space War', description: 'Best film of 1972')
-    video.save
-    expect(Video.first.title).to eq('Space War')
-  end
 
-  it "belongs to a category" do
-    scifi = Category.create(name: 'Sci-Fi')
-    space_war = Video.create(title: 'Space War',
-      description: 'Best film of 1972',
-      category: scifi)
-    expect(space_war.category).to eq(scifi)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 
-  it "requires a title" do
-    space_war = Video.new(description: 'Best film of 1972')
-    space_war.valid?
-    expect(space_war.errors.messages[:title]).to include("can't be blank")
-  end
-
-  it "requires a description" do
-    space_war = Video.new(title: 'Space War')
-    space_war.valid?
-    expect(space_war.errors.messages[:description]).to include("can't be blank")
-  end
 end
