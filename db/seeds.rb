97bc9ac6c13ea8cfaf44992970741c0e4ba828f4
Chris Reid <chris@raygun6.com>
2015-08-13 16:24:30 -0700

    show only recent videos on home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,28 @@ Video.create(title: 'South Park',
   small_cover_url: '',
   large_cover_url: '',
   category: Category.find(1))
+Video.create(title: 'Family Guy',
+  description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+  small_cover_url: '',
+  large_cover_url: '',
+  category: Category.find(1))
+Video.create(title: 'Futurama',
+  description: 'Another funny show. Please insert girder.',
+  small_cover_url: '',
+  large_cover_url: '',
+  category: Category.find(2))
+Video.create(title: 'Monk',
+  description: 'Never watched this one.',
+  small_cover_url: '',
+  large_cover_url: '',
+  category: Category.find(2))
+Video.create(title: 'South Park',
+  description: 'Is this show still on?',
+  small_cover_url: '',
+  large_cover_url: '',
+  category: Category.find(1))
+Video.create(title: 'South Park',
+  description: 'Is this show still on?',
+  small_cover_url: '',
+  large_cover_url: '',
+  category: Category.find(1))
