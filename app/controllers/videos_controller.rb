f4d31c2cf70909745b383ca80238446a99bddfd4
Chris Reid <chris@raygun6.com>
2015-08-13 15:22:38 -0700

    implements search in the ui

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,14 +8,18 @@ class VideosController < ApplicationController
   def show
   end
 
+  def search
+    @results = Video.search_by_title(params[:search_term])
+  end
+
   private
 
   def set_video
     @video = Video.find(params[:id])
   end
 
-  # def post_params
-  #   params.require(:video).permit(:id)
-  # end
+  def post_params
+    params.require(:video).permit(:id,:search_term)
+  end
 
 end
