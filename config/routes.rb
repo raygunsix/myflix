2e381875adca47e81155d69a72e9c7fe687b69a7
Chris Reid <chris@raygun6.com>
2015-08-17 15:17:01 -0700

    adds user model, users controller and front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home', to: 'videos#index'
+  get 'front', to: 'users#front'
 
   resources :videos, only: [:index, :show] do
     collection do
