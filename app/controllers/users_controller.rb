3f1ff86209ba39401cbe3f90e87b002aeeb55c47
Chris Reid <chris@raygun6.com>
2015-09-10 13:30:24 -0700

    adds new and create actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(post_params)
+    binding.pry
+    if @user.save
+      flash[:notice] = 'You have successfully regsitered!'
+      redirect_to home_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:fullname, :password, :username)
   end
 
 end
