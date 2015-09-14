cb8704bfefe6eb2f360a55ffb329e01a8b57a5f1
Chris Reid <chris@raygun6.com>
2015-09-14 11:46:11 -0700

    adds sign_in route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Myflix::Application.routes.draw do
   get 'front', to: 'users#front'
   get 'home', to: 'videos#index'
   get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
 
   resources :videos, only: [:index, :show] do
     collection do
