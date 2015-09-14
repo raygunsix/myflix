7c1ca831721f66b8058004025c17fc478556f755
Chris Reid <chris@raygun6.com>
2015-09-14 15:39:15 -0700

    allows a user to sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      login_user!(user)
+    else
+      flash[:error] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def login_user!(user)
+    session[:user_id] = user.id
+    flash[:notice] = 'Login successful'
+    redirect_to home_path
+  end
 end
