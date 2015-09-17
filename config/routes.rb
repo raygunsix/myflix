e749b3746325a4f53e7134a8e4513747995a893a
Chris Reid <chris@raygun6.com>
2015-09-17 10:40:38 -0700

    fixes error in route for front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
   get 'ui(/:action)', controller: 'ui'
-  get 'front', to: 'users#front'
+  get 'front', to: 'pages#front'
   get 'home', to: 'videos#index'
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
