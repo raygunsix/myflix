4cade9beba4780cffce3502c0fc909ac00f2e0a2
Chris Reid <chris@raygun6.com>
2015-09-25 16:31:09 -0700

    uses let instead of instance var

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -4,29 +4,32 @@ describe VideosController do
   before(:each) do
     user = Fabricate(:user)
     session[:user_id] = user.id
-    @family_guy = Fabricate(:video)
   end
 
   describe 'GET show' do
+    let(:family_guy) { Fabricate(:video) }
+
     it 'sets the @video instance variable' do
-      get :show, id: @family_guy.id
+      get :show, id: family_guy.id
       assigns(:video).should == Video.first
     end
 
     it 'renders the show template' do
-      get :show, id: @family_guy.id
+      get :show, id: family_guy.id
       response.should render_template :show
     end
   end
 
   describe "GET search" do
+    let(:family_guy) { Fabricate(:video) }
+
     it 'sets the @results instance variable' do
-      get :search, search_term: @family_guy.title
+      get :search, search_term: family_guy.title
       assigns(:results).should include Video.first
     end
 
     it 'renders the search template' do
-      get :search, search_term: @family_guy.title
+      get :search, search_term: family_guy.title
       response.should render_template :search
     end
   end
