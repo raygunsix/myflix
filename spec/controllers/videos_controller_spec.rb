ca8fe64df788212ed1304be0de6c036b83ff47a7
Chris Reid <chris@raygun6.com>
2015-09-25 17:18:38 -0700

    simplifys test variable naming

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,31 +3,31 @@ require 'spec_helper'
 describe VideosController do
 
   describe 'GET show' do
-    let(:family_guy) { Fabricate(:video) }
+    let(:video) { Fabricate(:video) }
 
     it 'sets the @video instance variable for authenticated users' do
       session[:user_id] = Fabricate(:user).id
-      get :show, id: family_guy.id
-      assigns(:video).should == Video.first
+      get :show, id: video.id
+      assigns(:video).should == video
     end
 
     it 'redirects to the sign in page for unauthenticated users' do
-      get :show, id: family_guy.id
+      get :show, id: video.id
       response.should redirect_to sign_in_path
     end
   end
 
   describe "GET search" do
-    let(:family_guy) { Fabricate(:video) }
+    let(:video) { Fabricate(:video) }
 
     it 'sets the @results instance variable for authenticated users' do
       session[:user_id] = Fabricate(:user).id
-      get :search, search_term: family_guy.title
-      assigns(:results).should include Video.first
+      get :search, search_term: video.title
+      assigns(:results).should include video
     end
 
     it 'redirects to the sign in page for unauthenticated users' do
-      get :search, search_term: family_guy.title
+      get :search, search_term: video.title
       response.should redirect_to sign_in_path
     end
   end
