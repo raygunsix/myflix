91165dbbbdbac43d7978c3c3c7d230d14258673b
Chris Reid <chris@raygun6.com>
2015-09-25 17:09:02 -0700

    removes redundant tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -13,11 +13,6 @@ describe VideosController do
       get :show, id: family_guy.id
       assigns(:video).should == Video.first
     end
-
-    it 'renders the show template' do
-      get :show, id: family_guy.id
-      response.should render_template :show
-    end
   end
 
   describe "GET search" do
@@ -26,11 +21,6 @@ describe VideosController do
     it 'sets the @results instance variable' do
       get :search, search_term: family_guy.title
       assigns(:results).should include Video.first
-    end
-
-    it 'renders the search template' do
-      get :search, search_term: family_guy.title
-      response.should render_template :search
     end
   end
 
