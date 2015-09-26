7d962fd305a8098beaceba700b19fc749b99f62f
Chris Reid <chris@raygun6.com>
2015-09-25 17:15:11 -0700

    adds specs for unauthenticated users

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper'
 
 describe VideosController do
-  before(:each) do
-    session[:user_id] = Fabricate(:user).id
-  end
 
   describe 'GET show' do
     let(:family_guy) { Fabricate(:video) }
 
-    it 'sets the @video instance variable' do
+    it 'sets the @video instance variable for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
       get :show, id: family_guy.id
       assigns(:video).should == Video.first
+    end
+
+    it 'redirects to the sign in page for unauthenticated users' do
+      get :show, id: family_guy.id
+      response.should redirect_to sign_in_path
     end
   end
 
   describe "GET search" do
     let(:family_guy) { Fabricate(:video) }
 
-    it 'sets the @results instance variable' do
+    it 'sets the @results instance variable for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
       get :search, search_term: family_guy.title
       assigns(:results).should include Video.first
     end
+
+    it 'redirects to the sign in page for unauthenticated users' do
+      get :search, search_term: family_guy.title
+      response.should redirect_to sign_in_path
+    end
   end
 
-  after(:each) do
-    session[:user_id] = nil
-  end
 end
