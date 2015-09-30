8e9d32275e5cdef85d3565af6a9e859fa9231a97
Chris Reid <chris@raygun6.com>
2015-09-30 16:45:38 -0700

    fabricates category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,29 +5,26 @@ describe Category do
   it { should have_many(:videos) }
 
   describe "recent videos" do
+    let(:comedies) { Fabricate(:category) }
 
     it "returns most recently created videos first" do
-      comedies = Category.create(name: 'comedies')
       family_guy = Video.create(title: "Family Guy", description: "Funny Show", category: comedies)
       family_ties = Video.create(title: "Family Ties", description: "Funny Show", category: comedies, created_at: 1.day.ago)
       comedies.recent_videos.first.created_at.should > comedies.recent_videos.last.created_at
     end
 
     it "returns only the most recent videos" do
-      comedies = Category.create(name: 'comedies')
       6.times { Video.create(title: "Family Guy", description: "Funny Show", category: comedies) }
       old_show = Video.create(title: "Leave it to Beaver", description: "Not a Funny Show", category: comedies, created_at: 1.day.ago)
       comedies.recent_videos.should_not include(old_show)
     end
 
     it "returns no more than six videos" do
-      comedies = Category.create(name: 'comedies')
       7.times { Video.create(title: "Family Guy", description: "Funny Show", category: comedies) }
       comedies.recent_videos.length.should == 6
     end
 
     it "returns an empty array if there are now videos" do
-      comedies = Category.create(name: 'comedies')
       comedies.recent_videos.should == []
     end
   end
