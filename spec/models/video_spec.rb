69f3cccd7ea47c0841accbcc66eacb9dd260a9b8
Chris Reid <chris@raygun6.com>
2015-10-02 16:03:02 -0700

    fabricates videos

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,24 +7,22 @@ describe Video do
   it { should validate_presence_of(:description) }
 
   describe "search by title" do
+    let!(:family_guy) { Fabricate(:video, title: 'Family Guy')}
+    let(:family_ties) { Fabricate(:video, title: 'Family Ties')}
 
-    it "returns an exmpty array if there is no match" do
-      Video.search_by_title("Family Guy").should == []
+    it "returns an empty array if there is no match" do
+      Video.search_by_title("Star Wars").should == []
     end
 
     it "returns an array of one video for an exact match" do
-      family_guy = Video.create(title: "Family Guy", description: "Funny Show")
       Video.search_by_title("Family Guy").length.should == 1
     end
 
     it "returns an array of one video for a partial match" do
-      family_guy = Video.create(title: "Family Guy", description: "Funny Show")
       Video.search_by_title("Fam").length.should == 1
     end
 
     it "returns an array of all matches ordered by created_at" do
-      family_ties = Video.create(title: "Family Ties", description: "Old Show")
-      family_guy = Video.create(title: "Family Guy", description: "Funny Show")
       Video.search_by_title("Fam").should match_array([family_ties, family_guy])
     end
   end
