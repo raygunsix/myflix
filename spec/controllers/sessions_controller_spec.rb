b689b9cb375e9d00d2bc10b002346c51fb0ec2cc
Chris Reid <chris@raygun6.com>
2015-10-08 11:54:46 -0700

    adds specs for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,35 +1,67 @@
 require 'spec_helper'
 
 describe SessionsController do
-  let!(:user) { Fabricate(:user) }
 
   describe 'GET new' do
-    it 'should redirect to the home page if the user is logged in' do
-      skip
-      session[:id] = user.id
-      get :new
-      response.should render :new
+    context 'with logged in user' do
+      let!(:user) { Fabricate(:user) }
+
+      it 'should redirect to the home page' do
+        session[:user_id] = user.id
+        get :new
+        response.should redirect_to home_path
+      end
+    end
+
+    context 'with logged out user' do
+      it 'should render the new template' do
+        get :new
+        response.should render_template :new
+      end
     end
   end
 
   describe 'POST create' do
-    it 'creates a session if the user supplies valid credentials' do
-      skip
-      post :create, user
-      session[:id].should == user.id
+
+    context 'with valid credentials' do
+      let!(:user) { Fabricate(:user) }
+      before { post :create, email: user.email, password: user.password }
+
+      it 'should log in the user' do
+        session[:user_id].should == user.id
+      end
+
+      it 'should redirect to the home page' do
+        response.should redirect_to home_path
+      end
     end
-    it 'redirects if the user supplies invalid credentials' do
-      skip
-      post :create, Fabricate(:user, email: nil)
-      response.should render :new
+
+    context 'with invalid credentials' do
+      let(:user) { Fabricate.attributes_for(:user, email: nil) }
+      before { post :create, user }
+
+      it 'should not log the user in' do
+        session[:user_id].should be_nil
+      end
+
+      it 'should render the new template' do
+        response.should render_template :new
+      end
     end
   end
 
   describe 'POST destroy' do
+    let!(:user) { Fabricate(:user) }
+
     it 'deletes the user session' do
-      skip
-      get :destroy, user.id
-      session[:id].should == nil
+      session[:user_id] == user.id
+      get :destroy
+      session[:user_id].should be_nil
+    end
+
+    it 'redirects the user' do
+      get :destroy
+      response.should redirect_to root_path
     end
   end
 
